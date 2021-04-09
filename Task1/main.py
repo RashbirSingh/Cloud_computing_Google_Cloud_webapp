a85dd78a84f6b31e69773e85e309193d72ba8036
Rashbir Kohli <rashbirkohli@gmail.com>
2021-04-09 22:33:48 +1000

    Initial Commit
    Assignment 1

---
+++ Task1/main.py
@@ -0,0 +1,274 @@
+from datetime import datetime
+from flask import Flask, render_template, render_template_string
+from flask import request, session
+from google.cloud import datastore, storage
+
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="static/s3810585assignment1-433bbcbb30a7.json"
+
+datastore_client = datastore.Client()
+
+app = Flask(__name__)
+app.secret_key = "hello"
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
+
+def download_blob(bucket_name, source_blob_name, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_blob_name)
+
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        kindUserInfo = "Task"
+        kindUserImg = "User"
+        req = request.form
+        id = req.get("id")
+        password = req.get("password")
+        key = datastore_client.key(kindUserInfo, id)
+        task = datastore_client.get(key)
+
+        if (task is not None) and (password == task["password"]):
+            session["CurrentActiveUser"] = id
+            session["CurrentActiveUserName"] = task['user_name']
+
+            # RETURN FORM
+            keyImg = datastore_client.key(kindUserImg, id)
+            taskImg = datastore_client.get(keyImg)
+
+            download_blob(taskImg["bucketname"],
+                          taskImg["userimage"],
+                          "static/userimage/"+taskImg["userimage"])
+
+            ##GETTING POST DATA
+            postquery = datastore_client.query(kind="postbox")
+            postquerylist = list(postquery.fetch(limit=10))
+
+            for postimageeach in postquerylist:
+                download_blob("s3810585-storage",
+                              postimageeach["img"],
+                              "static/postimage/" + postimageeach["img"])
+
+            return render_template('forum.html', user_name = session["CurrentActiveUser"],
+                                   userImageURL = "static/userimage/"+taskImg["userimage"],
+                                   postquerylist=postquerylist
+                                   )
+        else:
+            return render_template_string("ID or password is invalid")
+
+    else:
+        return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+
+        kindUserInfo = "Task"
+        kindUserImg = "User"
+        req = request.form
+
+        #Getting Form data
+        id = req.get("id")
+        password = req.get("password")
+        user_name = req.get("user_name")
+        img = request.files['file']
+        img.save(img.filename)
+
+        #Getting datastore client entity for user info
+        key = datastore_client.key(kindUserInfo, id)
+        getterTask = datastore_client.get(key)
+
+        #Getting datastore client entity for user image
+        keyImg = datastore_client.key(kindUserImg, id)
+        userImg = datastore.Entity(key=keyImg)
+
+        if getterTask is None:
+            task = datastore.Entity(key=key)
+            query = datastore_client.query(kind="Task")
+            query.add_filter("user_name", "=", user_name)
+            if len(list(query.fetch())) > 0:
+                return render_template_string("The username already exists")
+            else:
+                #Updaing entity
+                task["user_name"] = user_name
+                task["password"] = password
+                datastore_client.put(task)
+
+                userImg["bucketname"] = "s3810585-storage"
+                userImg["userimage"] = img.filename
+                datastore_client.put(userImg)
+
+                upload_blob("s3810585-storage", img.filename, img.filename)
+                os.remove(img.filename)
+
+                return render_template('login.html')
+        else:
+            return render_template_string("The ID already exists")
+
+    return render_template('register.html')
+
+@app.route('/userpage', methods=['GET', 'POST'])
+def userpage():
+    postquery = datastore_client.query(kind="postbox")
+    postquery.add_filter("id", "=", "s38105850")
+    postquerylist = list(postquery.fetch())
+    return render_template('userpage.html',
+                           postquerylist = postquerylist)
+
+
+@app.route('/editpost', methods=['GET', 'POST'])
+def editpost():
+    if request.method == "POST":
+        kindUserInfo = "Task"
+        kindUserImg = "User"
+        req = request.form
+        postid = req.get("postid")
+        postChangeKey = datastore_client.key('postbox',int(postid))
+        postChangeTask = datastore_client.get(postChangeKey)
+        return render_template("editpost.html", postChangeTask=postChangeTask)
+
+        # postChangeKey = datastore_client.key('postbox',postid )
+        # postChangeTask = datastore_client.get(postChangeKey)
+        # postChangeTask["id"] = id
+        # postChangeTask["subject"] = subject
+        # postChangeTask["msg"] = msg
+        # postChangeTask["user_names"] = session["CurrentActiveUser"]
+        # postChangeTask["datetime"] = datetime.now()
+        # postChangeTask["userImage"] = taskImg["userimage"]
+        # postChangeTask["img"] = img.filename
+
+    postquery = datastore_client.query(kind="postbox")
+    postquery.add_filter("id", "=", "s38105850")
+    postquerylist = list(postquery.fetch())
+    return render_template('userpage.html',
+                           postquerylist = postquerylist)
+
+
+@app.route('/pushchange', methods=['GET', 'POST'])
+def pushchange():
+    if request.method == "POST":
+        kindUserImg = "User"
+        req = request.form
+        postid = req.get("postid")
+        subject = req.get("subject")
+        msg = req.get("msg")
+        img = request.files['file']
+        img.save(img.filename)
+        upload_blob("s3810585-storage", img.filename, img.filename)
+
+        postChangeKey = datastore_client.key('postbox', int(postid))
+        postChangeTask = datastore_client.get(postChangeKey)
+        postChangeTask["subject"] = subject
+        postChangeTask["msg"] = msg
+        postChangeTask["img"] = img.filename
+        datastore_client.put(postChangeTask)
+
+        postquery = datastore_client.query(kind="postbox")
+        postquery.add_filter("id", "=", "s38105850")
+        postquerylist = list(postquery.fetch())
+
+
+        download_blob("s3810585-storage",
+                      postChangeTask["img"],
+                      "static/postimage/" + postChangeTask["img"])
+
+        return render_template('userpage.html',
+                               postquerylist=postquerylist)
+
+@app.route('/changepass', methods=['GET', 'POST'])
+def changepass():
+    if request.method == "POST":
+        kindUserInfo = "Task"
+        kindUserImg = "User"
+        id = session["CurrentActiveUser"]
+        req = request.form
+        oldPassword = req.get("oldpass")
+        newPassword = req.get("newpass")
+
+        changeKey = datastore_client.key(kindUserInfo,id )
+        changeTask = datastore_client.get(changeKey)
+
+        if changeTask["password"] == oldPassword:
+            changeTask["password"] = newPassword
+            datastore_client.put(changeTask)
+            return render_template('userpage.html')
+        else:
+            return render_template_string("The old password is incorrect")
+
+@app.route('/postarea', methods=['GET', 'POST'])
+def postarea():
+    if request.method == "POST":
+        kindPostBox = "postbox"
+        kindUserImg = "User"
+        req = request.form
+        subject = req.get("subject")
+        msg = req.get("msg")
+        img = request.files['file']
+        img.save(img.filename)
+        upload_blob("s3810585-storage", img.filename, img.filename)
+        id = session["CurrentActiveUser"]
+
+        keyImg = datastore_client.key(kindUserImg, id)
+        taskImg = datastore_client.get(keyImg)
+        download_blob(taskImg["bucketname"],
+                      taskImg["userimage"],
+                      "static/userimage/"+taskImg["userimage"])
+
+        keyPostBox = datastore_client.key(kindPostBox)
+        taskPostBox = datastore.Entity(key=keyPostBox)
+        taskPostBox["id"] = id
+        taskPostBox["subject"] = subject
+        taskPostBox["msg"] = msg
+        taskPostBox["user_names"] = session["CurrentActiveUser"]
+        taskPostBox["datetime"] = datetime.now()
+        taskPostBox["userImage"] = taskImg["userimage"]
+        taskPostBox["img"] = img.filename
+
+        datastore_client.put(taskPostBox)
+
+        ##GETTING POST DATA
+        postquery = datastore_client.query(kind="postbox")
+        postquerylist = list(postquery.fetch(limit=10))
+
+        for postimageeach in postquerylist:
+            download_blob("s3810585-storage",
+                          postimageeach["img"],
+                          "static/postimage/"+postimageeach["img"])
+
+        return render_template('forum.html', user_name=session["CurrentActiveUser"],
+                               userImageURL="static/userimage/" + taskImg["userimage"],
+                               postquerylist=postquerylist)
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop("CurrentActiveUser", None)
+    session.pop("CurrentActiveUserName", None)
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
+
+kind = "Task"
+name = "s38105859"
+task_key = datastore_client.key(kind, name)
+task = datastore.Entity(key=task_key)
+task["user_name"] = "rashbir kohli9"
+task["password"] = "901234"
+datastore_client.put(task)
---
+++ Task2/main.py
@@ -0,0 +1,194 @@
+import os
+
+from flask import Flask, render_template
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="static/s3810585assignment1-433bbcbb30a7.json"
+
+from google.cloud import bigquery
+
+app = Flask(__name__)
+
+
+""" FIRST
+SELECT time_ref, SUM(sumValue) AS trade_value
+FROM
+(SELECT time_ref, account, SUM(value) AS sumValue
+FROM `s3810585assignment1.gsquarterlySeptember20.gsquarterlySeptember20`
+GROUP BY time_ref, account) 
+AS result1
+GROUP BY time_ref
+ORDER BY trade_value DESC 
+LIMIT 10
+"""
+
+
+""" SECOND
+SELECT country_label , product_type, sum(case when account = 'Imports' then sumValue else - sumValue end) AS trade_deficit_value, status
+FROM 
+(SELECT country_code, time_ref, product_type, account, status, SUM(value) AS sumValue
+FROM `s3810585assignment1.gsquarterlySeptember20.gsquarterlySeptember20`
+WHERE time_ref > 201400 AND time_ref < 201800 AND status = 'F' AND product_type = 'Goods'
+GROUP BY country_code, time_ref, account, product_type, status) 
+AS result2
+FULL JOIN `s3810585assignment1.country_classification.country_classification`
+ON `s3810585assignment1.country_classification.country_classification`.country_code = result2.country_code
+GROUP BY country_label, product_type, status
+ORDER BY trade_deficit_value DESC 
+LIMIT 50"""
+
+
+""" THIRD
+SELECT service_label, trade_surplus_value
+FROM `s3810585assignment1.services_classification.services_classification`
+,
+(SELECT trade_surplus_value
+FROM(
+(SELECT time_ref, SUM(sumValue) AS trade_value, country_code, code
+FROM
+(SELECT time_ref, account, SUM(value) AS sumValue, country_code, code
+FROM `s3810585assignment1.gsquarterlySeptember20.gsquarterlySeptember20`
+GROUP BY time_ref, account, country_code, code) 
+AS result1
+GROUP BY time_ref, country_code, code
+ORDER BY trade_value DESC 
+LIMIT 10) AS finalresult1
+
+FULL JOIN 
+
+(SELECT code, time_ref, country_code , product_type, sum(case when account = 'Export' then sumValue else - sumValue end) AS trade_surplus_value, status
+FROM 
+(SELECT country_code, time_ref, product_type, account, status, SUM(value) AS sumValue, code
+FROM `s3810585assignment1.gsquarterlySeptember20.gsquarterlySeptember20`
+WHERE time_ref > 201400 AND time_ref < 201800 AND status = 'F' AND product_type = 'Goods'
+GROUP BY country_code, time_ref, account, product_type, status, code) 
+AS result2
+GROUP BY country_code, product_type, status, time_ref, code
+ORDER BY trade_surplus_value DESC 
+LIMIT 50) As finalresult2
+ON finalresult1.time_ref = finalresult2.time_ref AND finalresult1.country_code = finalresult2.country_code)
+)
+AS Result3
+ORDER BY trade_surplus_value DESC 
+LIMIT 30"""
+
+
+@app.route('/one')
+def one():
+    resultlist = []
+    client = bigquery.Client()
+    query_job = client.query(
+        """
+        SELECT time_ref, SUM(sumValue) AS trade_value
+        FROM
+        (SELECT time_ref, account, SUM(value) AS sumValue
+        FROM `s3810585assignment1.gsquarterlySeptember20.gsquarterlySeptember20`
+        GROUP BY time_ref, account) 
+        AS result1
+        GROUP BY time_ref
+        ORDER BY trade_value DESC 
+        LIMIT 10"""
+    )
+
+    results = query_job.result()  # Waits for job to complete.
+
+    for row in results:
+        resultlist.append({"time_ref":row.time_ref,
+                      "trade_value":row.trade_value})
+        print(row)
+
+    return render_template('resultone.html',
+                           answer="Answer 1",
+                           results=resultlist)
+
+
+@app.route('/two')
+def two():
+    resultlist = []
+    client = bigquery.Client()
+    query_job = client.query(
+        """
+        SELECT country_label , product_type, sum(case when account = 'Imports' then sumValue else - sumValue end) AS trade_deficit_value, status
+        FROM 
+        (SELECT country_code, time_ref, product_type, account, status, SUM(value) AS sumValue
+        FROM `s3810585assignment1.gsquarterlySeptember20.gsquarterlySeptember20`
+        WHERE time_ref > 201400 AND time_ref < 201800 AND status = 'F' AND product_type = 'Goods'
+        GROUP BY country_code, time_ref, account, product_type, status) 
+        AS result2
+        FULL JOIN `s3810585assignment1.country_classification.country_classification`
+        ON `s3810585assignment1.country_classification.country_classification`.country_code = result2.country_code
+        GROUP BY country_label, product_type, status
+        ORDER BY trade_deficit_value DESC 
+        LIMIT 50"""
+    )
+
+    results = query_job.result()  # Waits for job to complete.
+
+    for row in results:
+        resultlist.append({"country_label":row.country_label,
+                              "product_type":row.product_type,
+                              "trade_deficit_value": row.trade_deficit_value,
+                              "status":row.status})
+        print(row)
+
+    return render_template('resulttwo.html',
+                           answer="Answer 2",
+                           results=resultlist)
+
+
+@app.route('/three')
+def three():
+    resultlist = []
+    client = bigquery.Client()
+    query_job = client.query(
+        """
+        SELECT service_label, trade_surplus_value
+        FROM `s3810585assignment1.services_classification.services_classification`
+        ,
+        (SELECT trade_surplus_value
+        FROM(
+        (SELECT time_ref, SUM(sumValue) AS trade_value, country_code, code
+        FROM
+        (SELECT time_ref, account, SUM(value) AS sumValue, country_code, code
+        FROM `s3810585assignment1.gsquarterlySeptember20.gsquarterlySeptember20`
+        GROUP BY time_ref, account, country_code, code) 
+        AS result1
+        GROUP BY time_ref, country_code, code
+        ORDER BY trade_value DESC 
+        LIMIT 10) AS finalresult1
+        
+        FULL JOIN 
+        
+        (SELECT code, time_ref, country_code , product_type, sum(case when account = 'Export' then sumValue else - sumValue end) AS trade_surplus_value, status
+        FROM 
+        (SELECT country_code, time_ref, product_type, account, status, SUM(value) AS sumValue, code
+        FROM `s3810585assignment1.gsquarterlySeptember20.gsquarterlySeptember20`
+        WHERE time_ref > 201400 AND time_ref < 201800 AND status = 'F' AND product_type = 'Goods'
+        GROUP BY country_code, time_ref, account, product_type, status, code) 
+        AS result2
+        GROUP BY country_code, product_type, status, time_ref, code
+        ORDER BY trade_surplus_value DESC 
+        LIMIT 50) As finalresult2
+        ON finalresult1.time_ref = finalresult2.time_ref AND finalresult1.country_code = finalresult2.country_code)
+        )
+        AS Result3
+        ORDER BY trade_surplus_value DESC 
+        LIMIT 30"""
+    )
+
+    results = query_job.result()  # Waits for job to complete.
+
+    for row in results:
+        resultlist.append({"service_label":row.service_label,
+                      "trade_surplus_value":row.trade_surplus_value})
+        print(row)
+
+    return render_template('resultthree.html',
+                           answer="Answer 3",
+                           results=resultlist)
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080, debug=True)
